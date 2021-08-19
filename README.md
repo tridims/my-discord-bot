6957d25a97bb0c73ab534aa545144a42454cae7e
Dimas Tri <test31122001@gmail.com>
2021-08-19 11:28:49 +0700

    mod readme, add idea

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# my-discord-bot
+# My Discord Bot Project
+
+This is my discord bot project. The idea of this bot is that it can save notes from discord from specific user by using some keyword. It can also paste the note back by using some keywords too. And than i want to create specific frontend website to view those note too.
--- bot.py
+++ bot.py
@@ -16,21 +16,21 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-    print('Airy Joined the following Guilds : ')
+    print(f'{bot.user} Joined the following Guilds : ')
     for guild in bot.guilds:
-        print(f' <*> {guild.name} with id {guild.id}')
+        print(
+            f'<*> {guild.name} with id {guild.id} with {len(guild.members)} members')
         members = '\n - '.join([member.name for member in guild.members])
-        print(f'Number of members : {len(guild.members)}')
-        print(f'Guild Members:\n - {members}')
+        print(f'< > Guild Members:\n     - {members}')
 
 
-@bot.command(name='hello')
+@bot.command(name='hello' or 'Hello')
 async def hello(ctx):
     await ctx.send("Hello Too !!")
 
 
-@bot.command(name='roll-dice', help="Simulates rolling dice")
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
+@bot.command(name='roll-dice', help="Simulates rolling dice, you can put custom number of dice, and number of sides")
+async def roll(ctx, number_of_dice: int = 1, number_of_sides: int = 6):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
         for _ in range(number_of_dice)
@@ -46,6 +46,12 @@ async def create_channel(ctx, channel_name: str):
     if not existing_channel:
         print(f'Creating new channel named {channel_name}')
         await guild.create_text_channel(channel_name)
+
+
+@bot.command(name='add-note', help='add text to your note')
+async def add_note(ctx, text_note: str):
+    people = ctx.user
+    ctx.send(f'{people} creating note : {text_note}')
 
 
 @bot.event
